0c88fa2cbaa906498e8d75335d57b57abc7b5f8d
glealnunes <gpfilho2010@gmail.com>
2024-04-18 13:20:34 -0300

    env - refatoração de estilo, adição do optimize imagem

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
-
+import { NgOptimizedImage } from '@angular/common';
+import { InformacoesComponent } from '../informacoes/informacoes.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [
+    HeaderComponent,
+    NgOptimizedImage,
+    InformacoesComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
