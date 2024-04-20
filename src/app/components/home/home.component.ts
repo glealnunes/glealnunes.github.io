79516958f2770a2e18985671c86c93a440becb8f
glealnunes <gpfilho2010@gmail.com>
2024-04-20 09:41:28 -0300

    feat: adição da pagina serviços

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { NgOptimizedImage } from '@angular/common';
 import { InformacoesComponent } from '../informacoes/informacoes.component';
+import { ServicosComponent } from '../servicos/servicos.component';
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     HeaderComponent,
     NgOptimizedImage,
-    InformacoesComponent
+    InformacoesComponent,
+    ServicosComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
---
+++ src/app/components/servicos/servicos.component.ts
@@ -0,0 +1,15 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servicos',
+  standalone: true,
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './servicos.component.html',
+  styleUrl: './servicos.component.scss'
+})
+export class ServicosComponent {
+
+}
