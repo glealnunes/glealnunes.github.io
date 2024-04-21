84b7c842faeef3798d2399a702f88297d54d53ee
glealnunes <gpfilho2010@gmail.com>
2024-04-21 16:27:02 -0300

    feat : adição da pagina de tecnologias

---
+++ src/app/components/tecnologia/tecnologia.component.ts
@@ -0,0 +1,15 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tecnologia',
+  standalone: true,
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './tecnologia.component.html',
+  styleUrl: './tecnologia.component.scss'
+})
+export class TecnologiaComponent {
+
+}
