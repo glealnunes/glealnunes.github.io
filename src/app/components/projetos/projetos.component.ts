aed77884c3f7332f6774a02a7d0790333bc4becf
glealnunes <gpfilho2010@gmail.com>
2024-05-08 18:00:29 -0300

    feat : adição da pagina de projetos

---
+++ src/app/components/projetos/projetos.component.ts
@@ -0,0 +1,15 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projetos',
+  standalone: true,
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './projetos.component.html',
+  styleUrl: './projetos.component.scss'
+})
+export class ProjetosComponent {
+
+}
