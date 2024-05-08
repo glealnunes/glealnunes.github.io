5b1bdf25d1ea8f997ab619c363a1468c954ba635
glealnunes <gpfilho2010@gmail.com>
2024-05-08 18:01:00 -0300

    feat : adição da pagina de contato

---
+++ src/app/components/contato/contato.component.ts
@@ -0,0 +1,15 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contato',
+  standalone: true,
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './contato.component.html',
+  styleUrl: './contato.component.scss'
+})
+export class ContatoComponent {
+
+}
